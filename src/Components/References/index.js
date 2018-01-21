41152ce35bd4e951ce933c1b1e5dd982f5cf1a66
Unknown <ehellblom@outlook.com>
2018-01-21 18:04:48 +0100

    Add references page

---
+++ src/Components/References/index.js
@@ -0,0 +1,64 @@
+//Dependencies
+import React from 'react';
+//Internals
+import Navbar from '../Navbar';
+import './styles.css';
+
+const References = () => (
+  <div className="references-list">
+    <Navbar />
+    {/* CARD */}
+    <div className="card">
+      <img className="card-img-top" src="https://static.pexels.com/photos/428339/pexels-photo-428339.jpeg" alt="Card cap" />
+      <div className="card-body">
+        <h4 className="card-title">This Guy</h4>
+        <h5 className="card-info"></h5>
+        <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        <hr />
+        <p id="email">Email: example@gmail.com</p>
+      </div>
+    </div>
+    {/* CARD */}
+    <div className="card">
+      <img className="card-img-top" src="https://static.pexels.com/photos/428339/pexels-photo-428339.jpeg" alt="Card cap" />
+      <div className="card-body">
+        <h4 className="card-title">That Guy</h4>
+        <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        <hr />
+        <p id="email">Email: example@gmail.com</p>
+      </div>
+    </div>
+    {/* CARD */}
+    <div className="card">
+      <img className="card-img-top" src="https://static.pexels.com/photos/428339/pexels-photo-428339.jpeg" alt="Card cap" />
+      <div className="card-body">
+        <h4 className="card-title">Contact</h4>
+        <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        <hr />
+        <p id="email">Email: example@gmail.com</p>
+      </div>
+    </div>
+    {/* CARD */}
+    <div className="card">
+      <img className="card-img-top" src="https://static.pexels.com/photos/428339/pexels-photo-428339.jpeg" alt="Card cap" />
+      <div className="card-body">
+        <h4 className="card-title">Contact</h4>
+        <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        <hr />
+        <p id="email">Email: example@gmail.com</p>
+      </div>
+    </div>
+    {/* CARD */}
+    <div className="card">
+      <img className="card-img-top" src="https://static.pexels.com/photos/428339/pexels-photo-428339.jpeg" alt="Card cap" />
+      <div className="card-body">
+        <h4 className="card-title">Contact</h4>
+        <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        <hr />
+        <p id="email">Email: example@gmail.com</p>
+      </div>
+    </div>
+  </div>
+)
+
+export default References;
